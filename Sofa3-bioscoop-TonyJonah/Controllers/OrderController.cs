3e6d15a8f1a26b595f039e569b2a57e505718141
Jonah Eikenhorst <jonah.eikenhorst@gmail.com>
2024-02-06 14:26:37 +0100

    add: UnitTests

--- Sofa3-bioscoop-TonyJonah/Controllers/OrderController.cs
+++ Sofa3-bioscoop-TonyJonah/Controllers/OrderController.cs
@@ -1,56 +1,56 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace Sofa3_bioscoop_TonyJonah.Controllers
-{
-    [ApiController]
-    [Route("[controller]")]
-    public class OrderController : ControllerBase
-    {
-        [HttpGet(Name = "test")]
-        public void Get()
-        {
-            Movie movie = new Movie("The Matrix");
-            Movie otherMovie = new Movie("John Wick");
-
-// Create a non-weekend movie screening
-            DateTime wednesdayDateTime = new DateTime(2024, 1, 31, 19, 0, 0); // January 31, 2024 is a Wednesday
-            MovieScreening movieScreening = new MovieScreening(movie, wednesdayDateTime, 10.0M);
-
-// Create a weekend movie screening
-            DateTime saturdayDateTime = new DateTime(2024, 2, 3, 19, 0,0    ); // February 3, 2024 is a Saturday
-            MovieScreening weekendMovieScreening = new MovieScreening(movie, saturdayDateTime, 10.0M);
-
-// Add the screenings to the movie
-            movie.addScreening(movieScreening);
-            otherMovie.addScreening(weekendMovieScreening);
-
-// Create a non-premium movie ticket
-            MovieTicket movieTicket = new MovieTicket(movieScreening, 1, 1, false);
-
-// Create a premium movie ticket
-            MovieTicket premiumMovieTicket = new MovieTicket(movieScreening, 1, 1, true);
-
-// Create a non-student order
-            Order order = new Order(1);
-            order.AddSeatReservation(premiumMovieTicket);
-            order.AddSeatReservation(movieTicket); // This ticket should be free because it's the second ticket
-
-// Create a student order
-            Order studentOrder = new Order(2);
-            studentOrder.AddSeatReservation(premiumMovieTicket);
-            studentOrder.AddSeatReservation(movieTicket); // This ticket should be free because it's the second
-
-// Create a non-student order with 6 tickets for a weekend screening
-            Order groupOrder = new Order(3);
-            for (int i = 0; i < 6; i++)
-                groupOrder.AddSeatReservation(new MovieTicket(weekendMovieScreening, 1, i + 1, true));
-            
-// Export the orders
-            order.Export(TicketExportFormat.PLAINTEXT);
-            studentOrder.Export(TicketExportFormat.JSON);
-            groupOrder.Export(TicketExportFormat.PLAINTEXT);
-        }
-    }
-
-
-}
+﻿// using Microsoft.AspNetCore.Mvc;
+//
+// namespace Sofa3_bioscoop_TonyJonah.Controllers
+// {
+//     [ApiController]
+//     [Route("[controller]")]
+//     public class OrderController : ControllerBase
+//     {
+//         [HttpGet(Name = "test")]
+//         public void Get()
+//         {
+//             Movie movie = new Movie("The Matrix");
+//             Movie otherMovie = new Movie("John Wick");
+//
+// // Create a non-weekend movie screening
+//             DateTime wednesdayDateTime = new DateTime(2024, 1, 31, 19, 0, 0); // January 31, 2024 is a Wednesday
+//             MovieScreening movieScreening = new MovieScreening(movie, wednesdayDateTime, 10.0M);
+//
+// // Create a weekend movie screening
+//             DateTime saturdayDateTime = new DateTime(2024, 2, 3, 19, 0,0    ); // February 3, 2024 is a Saturday
+//             MovieScreening weekendMovieScreening = new MovieScreening(movie, saturdayDateTime, 10.0M);
+//
+// // Add the screenings to the movie
+//             movie.addScreening(movieScreening);
+//             otherMovie.addScreening(weekendMovieScreening);
+//
+// // Create a non-premium movie ticket
+//             MovieTicket movieTicket = new MovieTicket(movieScreening, 1, 1, false);
+//
+// // Create a premium movie ticket
+//             MovieTicket premiumMovieTicket = new MovieTicket(movieScreening, 1, 1, true);
+//
+// // Create a non-student order
+//             Order order = new Order(1);
+//             order.AddSeatReservation(premiumMovieTicket);
+//             order.AddSeatReservation(movieTicket); // This ticket should be free because it's the second ticket
+//
+// // Create a student order
+//             Order studentOrder = new Order(2);
+//             studentOrder.AddSeatReservation(premiumMovieTicket);
+//             studentOrder.AddSeatReservation(movieTicket); // This ticket should be free because it's the second
+//
+// // Create a non-student order with 6 tickets for a weekend screening
+//             Order groupOrder = new Order(3);
+//             for (int i = 0; i < 6; i++)
+//                 groupOrder.AddSeatReservation(new MovieTicket(weekendMovieScreening, 1, i + 1, true));
+//             
+// // Export the orders
+//             order.Export(TicketExportFormat.PLAINTEXT);
+//             studentOrder.Export(TicketExportFormat.JSON);
+//             groupOrder.Export(TicketExportFormat.PLAINTEXT);
+//         }
+//     }
+//
+//
+// }
--- bioscoopTestProject/OrderTests.cs
+++ bioscoopTestProject/OrderTests.cs
@@ -1,11 +1,69 @@
+using Sofa3_bioscoop_TonyJonah;
+
 namespace bioscoopTestProject
 {
     public class OrderTests
     {
         [Fact]
-        public void Test1()
+        public void CalculatePrice_SecondTicketFreeForStudent()
         {
+            Order order = CreateStudentOrder(6, 10M, true, true);
+            decimal price = order.CalculatePrice();
 
+            Assert.Equal(36M, price);
+
+        }
+        
+        [Fact]
+        public void CalculatePrice_10ProcentDiscounForNonStudents()
+        {
+            Order order = CreateOrder(7, 10M, true, true);
+            decimal price = order.CalculatePrice();
+
+            Assert.Equal(63M, price);
+
+        }
+        
+        [Fact]
+        public void CalculatePrice_PremiumTicketShouldBeTwoExtraForStudentInPrice()
+        {
+            Order order = CreateStudentOrder(1, 10M, true, true);
+            decimal price = order.CalculatePrice();
+            
+            Assert.Equal(12M, price);
+        }
+
+        [Fact]
+        public void CalculatePrice_NonPremiumTicketShouldBeBasePrice()
+        {
+            Order order = CreateStudentOrder(1, 10M, false, true);
+            decimal price = order.CalculatePrice();
+            
+            Assert.Equal(10M, price);
+        }
+        
+        private static Order CreateStudentOrder(int numberOfTickets, decimal basePrice, bool isPremium, bool isWeekend)
+        {
+            Movie movie = new Movie("The Matrix");
+            
+            DateTime date = isWeekend ? new DateTime(2024, 1, 27, 19, 0, 0) : new DateTime(2024, 1, 31, 19, 0, 0);
+            MovieScreening movieScreening = new MovieScreening(movie, date, basePrice);
+            Order order = new Order(1);
+            for (int i = 0; i < numberOfTickets; i++)
+                order.AddSeatReservation(new StudentMovieTicket(movieScreening, 1, 1, isPremium));
+            return order;
+        }
+        
+        private static Order CreateOrder(int numberOfTickets, decimal basePrice, bool isPremium, bool isWeekend)
+        {
+            Movie movie = new Movie("The Matrix");
+            
+            DateTime date = isWeekend ? new DateTime(2024, 1, 27, 19, 0, 0) : new DateTime(2024, 1, 31, 19, 0, 0);
+            MovieScreening movieScreening = new MovieScreening(movie, date, basePrice);
+            Order order = new Order(1);
+            for (int i = 0; i < numberOfTickets; i++)
+                order.AddSeatReservation(new MovieTicket(movieScreening, 1, 1, isPremium));
+            return order;
         }
     }
 }
